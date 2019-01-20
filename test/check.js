3ae45c200681e600e713041b43f497f7edc189db
Brian Boucheron <brian@boucheron.org>
2019-01-20 08:27:07 -0500

    add function validation and tests

---
+++ test/check.js
@@ -0,0 +1,205 @@
+require('tap').mochaGlobals()
+const assert = require('assert')
+
+const {
+  check,
+  isObject,
+  isNumber,
+  isString,
+  hasSomeKeys,
+  hasKeys,
+  checkKeys,
+} = require('../lib/check.js')
+
+describe('check()', function() {
+  it('throws when a test fails', function() {
+    assert.throws(() => check('', isNumber), /^TypeError/)
+  })
+
+  it('does nothing when tests pass', function() {
+    assert.doesNotThrow(() => check('a', isString))
+  })
+
+  it('runs multiple tests when passed a list', function() {
+    assert.doesNotThrow(() => check({ a: 1 }, [isObject, hasKeys('a')]))
+  })
+})
+
+describe('isObject()', function() {
+  it('throws on array', function() {
+    assert.throws(() => isObject([]), /^TypeError/)
+  })
+
+  it('throws on string', function() {
+    assert.throws(() => isObject(''), /^TypeError/)
+  })
+
+  it('throws on number', function() {
+    assert.throws(() => isObject(0), /^TypeError/)
+  })
+
+  it('throws on function', function() {
+    assert.throws(() => isObject(function() {}), /^TypeError/)
+  })
+
+  it("does not throw on {a: ''}", function() {
+    assert.doesNotThrow(() => isObject({ a: '' }))
+  })
+})
+
+describe('hasSomeKeys()', function() {
+  it('throws if a key is not found', function() {
+    assert.throws(function() {
+      hasSomeKeys(['x', 'y'])({ z: 0 })
+    }, /^TypeError/)
+  })
+
+  it('does not throw if a key is found', function() {
+    assert.doesNotThrow(function() {
+      hasSomeKeys(['x', 'y'])({ x: 0 })
+    })
+  })
+
+  it('accepts keys as a list of strings', function() {
+    assert.doesNotThrow(function() {
+      hasSomeKeys(['x', 'y'])({ x: 0 })
+    })
+  })
+
+  it('ignores extraneous keys', function() {
+    assert.doesNotThrow(function() {
+      hasSomeKeys(['x', 'y'])({ x: 0, z: 0 })
+    })
+  })
+})
+
+describe('hasKeys()', function() {
+  it('throws if any key is not found', function() {
+    assert.throws(function() {
+      hasKeys(['x', 'y'])({ x: 0 })
+    }, /^TypeError/)
+  })
+
+  it('does not throw if all keys are found', function() {
+    assert.doesNotThrow(function() {
+      hasKeys(['x', 'y'])({ x: 0, y: 0 })
+    })
+  })
+
+  it('accepts a key as a string', function() {
+    assert.doesNotThrow(function() {
+      hasKeys('x')({ x: 0 })
+    })
+  })
+
+  it('accepts keys as a list of strings', function() {
+    assert.doesNotThrow(function() {
+      hasKeys(['x', 'y'])({ x: 0, y: 0 })
+    })
+  })
+
+  it('ignores extraneous keys', function() {
+    assert.doesNotThrow(function() {
+      hasKeys(['x', 'y'])({ x: 0, y: 0, z: 0 })
+    })
+  })
+})
+
+describe('isNumber()', function() {
+  const rejects = ['string', {}, []]
+
+  rejects.forEach((test) => {
+    it('rejects non numbers: ' + JSON.stringify(test), function() {
+      assert.throws(() => isNumber(test), /^TypeError/)
+    })
+  })
+
+  const accepts = [
+    0,
+    '0',
+    1,
+    '1',
+    0.1,
+    '0.1',
+    -1,
+    '-1',
+    999999,
+    '99999',
+    1e5,
+    '1e5',
+  ]
+
+  accepts.forEach((test) => {
+    it('accepts numbers: ' + JSON.stringify(test), function() {
+      assert.doesNotThrow(() => isNumber(test))
+    })
+  })
+})
+
+describe('isString()', function() {
+  const rejects = [1, {}, []]
+
+  rejects.forEach((test) => {
+    it('rejects non strings: ' + JSON.stringify(test), function() {
+      assert.throws(() => isString(test), /^TypeError/)
+    })
+  })
+
+  const accepts = ['string', '🎉']
+
+  accepts.forEach((test) => {
+    it('accepts non-empty strings: ' + JSON.stringify(test), function() {
+      assert.doesNotThrow(() => isString(test))
+    })
+  })
+})
+
+describe('checkKeys()', function() {
+  it('throws if key does not match test', function() {
+    assert.throws(function() {
+      const test = checkKeys({
+        x: [isNumber],
+      })
+      test({ x: '' })
+    }, /^TypeError/)
+  })
+
+  it('does not throw if key passes test', function() {
+    assert.doesNotThrow(function() {
+      const test = checkKeys({
+        x: [isNumber],
+      })
+      test({ x: 0 })
+    })
+  })
+
+  it('handles multiple keys', function() {
+    assert.doesNotThrow(function() {
+      const test = checkKeys({
+        x: [isNumber],
+        y: [isObject],
+      })
+      test({ x: 0, y: { a: '' } })
+    })
+  })
+
+  it('ignores missing keys', function() {
+    assert.doesNotThrow(function() {
+      const test = checkKeys({
+        x: [isNumber],
+        y: [isObject],
+      })
+      test({ x: 0 })
+    })
+  })
+
+  it('allows key checks to not be a list', function() {
+    assert.doesNotThrow(function() {
+      const test = checkKeys({
+        x: isNumber,
+        y: isObject,
+      })
+      test({ x: 0 })
+    })
+  })
+})
---
+++ test/validate.js
@@ -0,0 +1,164 @@
+require('tap').mochaGlobals()
+const assert = require('assert')
+
+const validate = require('../lib/validate.js')
+
+describe('.xy()', function() {
+  const rejects = [
+    [, 'no argument'],
+    [{}, 'empty object'],
+    [{ z: 0 }, 'no x or y'],
+    [{ x: '', y: 0 }, 'x is not a number'],
+    [{ x: 0, y: '' }, 'y is not a number'],
+  ]
+
+  rejects.forEach((test) => {
+    it('throws on ' + test[1] + ': ' + JSON.stringify(test[0]), function() {
+      assert.throws(function() {
+        validate.xy(test[0])
+      }, /^TypeError/)
+    })
+  })
+
+  const accepts = [
+    [{ x: 0 }, 'only x'],
+    [{ y: -10 }, 'only y'],
+    [{ x: 10, y: -20 }, 'x and y'],
+    [{ x: '0', y: '1' }, 'x and y are numberable strings'],
+    [{ x: 0, q: '' }, 'additional values are ignored'],
+  ]
+
+  accepts.forEach((test) => {
+    it('accepts ' + test[1] + ': ' + JSON.stringify(test[0]), function() {
+      assert.doesNotThrow(function() {
+        validate.xy(test[0])
+      })
+    })
+  })
+})
+
+describe('.xyz()', function() {
+  const rejects = [
+    [, 'no argument'],
+    [{}, 'empty object'],
+    [{ q: 0 }, 'no x or y or z'],
+    [{ x: '', y: 0, z: 0 }, 'x is not a number'],
+    [{ x: 0, y: '', z: 0 }, 'y is not a number'],
+    [{ x: 0, y: 0, z: '' }, 'z is not a number'],
+  ]
+
+  rejects.forEach((test) => {
+    it('throws on ' + test[1] + ': ' + JSON.stringify(test[0]), function() {
+      assert.throws(function() {
+        validate.xyz(test[0])
+      }, /^TypeError/)
+    })
+  })
+
+  const accepts = [
+    [{ x: 0 }, 'only x'],
+    [{ y: -10 }, 'only y'],
+    [{ z: 10 }, 'only z'],
+    [{ x: 10, y: -20 }, 'x and y'],
+    [{ x: 10, y: -20, z: 10 }, 'x and y and z'],
+    [{ x: '0', y: '1', z: '2' }, 'x and y and z are numberable strings'],
+    [{ z: 0, q: '' }, 'additional values are ignored'],
+  ]
+
+  accepts.forEach((test) => {
+    it('accepts ' + test[1] + ': ' + JSON.stringify(test[0]), function() {
+      assert.doesNotThrow(function() {
+        validate.xyz(test[0])
+      })
+    })
+  })
+})
+
+describe('.cmd()', function() {
+  const rejects = [
+    [, 'no argument'],
+    [{}, 'empty object'],
+    [{ a: 0 }, 'no g'],
+    [{ g: 0 }, 'g is not a string'],
+    [{ g: 'G01', params: {} }, 'empty params object'],
+  ]
+
+  rejects.forEach((test) => {
+    it('throws on ' + test[1] + ': ' + JSON.stringify(test[0]), function() {
+      assert.throws(function() {
+        validate.cmd(test[0])
+      }, /^TypeError/)
+    })
+  })
+
+  const accepts = [
+    [{ g: 'G01' }, 'only g'],
+    [{ g: 'G01', params: { a: 'foo' } }, 'g plus params'],
+    [{ g: 'M255', params: { a: 'x', b: 'y' } }, 'multiple params'],
+  ]
+
+  accepts.forEach((test) => {
+    it('accepts ' + test[1] + ': ' + JSON.stringify(test[0]), function() {
+      assert.doesNotThrow(function() {
+        validate.cmd(test[0])
+      })
+    })
+  })
+})
+
+describe('.name()', function() {
+  const rejects = [
+    [, 'no argument'],
+    [1, 'number'],
+    [['name'], 'list'],
+    [{ name: '' }, 'object'],
+  ]
+
+  rejects.forEach((test) => {
+    it('throws on ' + test[1] + ': ' + JSON.stringify(test[0]), function() {
+      assert.throws(function() {
+        validate.name(test[0])
+      }, /^TypeError/)
+    })
+  })
+
+  const accepts = ['robit', 'sally', 'line-us']
+
+  accepts.forEach((test) => {
+    it('accepts ' + JSON.stringify(test), function() {
+      assert.doesNotThrow(function() {
+        validate.name(test)
+      })
+    })
+  })
+})
+
+describe('.wifi()', function() {
+  const rejects = [
+    [, 'no argument'],
+    [{}, 'empty object'],
+    [{ password: 'pass' }, 'no ssid'],
+    [{ ssid: 8 }, 'ssid is a number'],
+  ]
+
+  rejects.forEach((test) => {
+    it('throws on ' + test[1] + ': ' + JSON.stringify(test[0]), function() {
+      assert.throws(function() {
+        validate.wifi(test[0])
+      }, /^TypeError/)
+    })
+  })
+
+  const accepts = [
+    [{ ssid: 'hello' }, 'only ssid'],
+    [{ ssid: 'hello', password: 'pass' }, 'ssid and password'],
+  ]
+
+  accepts.forEach((test) => {
+    it('accepts ' + test[1] + ': ' + JSON.stringify(test[0]), function() {
+      assert.doesNotThrow(function() {
+        validate.wifi(test[0])
+      })
+    })
+  })
+})
